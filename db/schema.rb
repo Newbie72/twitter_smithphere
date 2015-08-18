80f86b64a77f8715a53204014eadc5946d6e9ce7
Terence Smith <terencesmith1@gmail.com>
2015-08-17 20:09:59 -0400

    Added Authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817230209) do
+ActiveRecord::Schema.define(version: 20150817231340) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string   "message"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
